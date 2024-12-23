81726a76e2953bf400e90b836992cff5e121a464
Ajay <ajaykrishna13@gmail.com>
2024-12-23 12:41:44 +0530

    Initial commit

---
+++ client/src/routes/signInPage/signInPage.jsx
@@ -0,0 +1,13 @@
+import { SignIn } from '@clerk/clerk-react'
+import './signInPage.css'
+const signInPage = () => {
+  return (
+    <div className='signInPage'>
+      <SignIn path="/sign-in" 
+      signUpUrl='/sign-up'
+       forceRedirectUrl="/dashboard"/>
+    </div>
+  )
+}
+
+export default signInPage
---
+++ client/src/routes/signUpPage/signUpPage.jsx
@@ -0,0 +1,11 @@
+import { SignUp } from '@clerk/clerk-react'
+import './signUpPage.css'
+const signUpPage = () => {
+  return (
+    <div className='signUpPage'>
+      <SignUp path="/sign-up" signInUrl='sign-in' />
+    </div>
+  )
+}
+
+export default signUpPage
